5752315ad086fe0c13d4d46fcf4e57316b39314a
Alexander Konovalov <alexk@mcs.st-andrews.ac.uk>
2019-02-22 23:06:07 +0000

    Update script for GAP-minor-release-win-build Jenkins build
    This is the version that works on gapw10-build machine

--- testing/stable/GAP-minor-release-win-build.sh
+++ testing/stable/GAP-minor-release-win-build.sh
@@ -11,7 +11,7 @@ rm -rf /cygdrive/c/${DISTNAME}
 cp -r $DISTNAME /cygdrive/c/${DISTNAME}
 rm -rf $DISTNAME
 cd /cygdrive/c/${DISTNAME}
-#./autogen.sh
+./autogen.sh
 ./configure
 make
 
@@ -35,7 +35,7 @@ sed -i 's/cygwin\\bin/'"${DISTNAME}"'\\bin\\i686-pc-cygwin-default32-kv3/g' gapr
 cd ..
 
 bin/instcygwinterminfo.sh
-cp /bin/cyggcc_s-1.dll /bin/cyggmp-10.dll /bin/cygncurses-10.dll /bin/cygncursesw-10.dll /bin/cygpanel-10.dll /bin/cygpanelw-10.dll /bin/cygpopt-0.dll /bin/cygreadline7.dll /bin/cygstart.exe /bin/cygstdc++-6.dll /bin/cygwin1.dll /bin/libW11.dll /bin/mintty.exe /usr/bin/cygz.dll /usr/bin/rxvt.exe /usr/bin/regtool.exe /usr/x86_64-w64-mingw32/sys-root/mingw/bin/zlib1.dll bin/i686-pc-cygwin-default32-kv3
+cp /bin/cyggcc_s-1.dll /bin/cyggmp-10.dll /bin/cygncursesw-10.dll /bin/cygpanelw-10.dll /bin/cygpopt-0.dll /bin/cygreadline7.dll /bin/cygstart.exe /bin/cygstdc++-6.dll /bin/cygwin1.dll /bin/mintty.exe /usr/bin/cygz.dll /usr/bin/rxvt.exe /usr/bin/regtool.exe /usr/i686-w64-mingw32/sys-root/mingw/bin/zlib1.dll bin/i686-pc-cygwin-default32-kv3
 cp /bin/cygcurl* /bin/cygidn* /bin/cygcrypto* /bin/cyggssapi* /bin/cyglber* /bin/cygldap* /bin/cygssh* /bin/cygssl* /bin/cygkrb* /bin/cygk5crypto* /bin/cygintl* /bin/cygcom_err* /bin/cygunistring* /bin/cygiconv* /bin/cygsasl* bin/i686-pc-cygwin-default32-kv3
 cd ..
 zip -qq -r $WORKSPACE/${ARCHNAME}-win.zip ${DISTNAME}
